2fe49982bed1c4d759679b28b4434717f19c7304
Drew Daigle <daigled1+github@gmail.com>
2023-03-09 19:56:08 -0500

    refactor(transfer-learning): move transfer learning example to its own file

--- 004-horses-or-humans.py
+++ 004-horses-or-humans.py
@@ -71,7 +71,7 @@ def test_images(model, kernel=(300, 300)):
             print(item + " is a horse")
 
 
-def image_augmentation():
+def main():
     callbacks = myCallback()
     
     # Fetch Training Data
@@ -165,82 +165,5 @@ def image_augmentation():
     test_images(model=model)
 
 
-def transfer_learning():
-    callbacks = myCallback()
-
-    # Call in pre-existing layers
-    weights_url = "https://storage.googleapis.com/mledu-datasets/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5"
-    weights_file = "inception_v3.h5"
-
-    urllib.request.urlretrieve(weights_url, weights_file)
-
-    # Load Existing Layers into model
-    pre_trained_model = InceptionV3(
-        input_shape=(150, 150, 3),
-        include_top=False,
-        weights=None
-    )
-
-    pre_trained_model.load_weights(weights_file)
-
-    # Summarize Pre-Trained Model
-    pre_trained_model.summary()
-
-    # Capture the last layer of output that we're interested in from the pre-existing model
-    last_layer = pre_trained_model.get_layer('mixed7')
-    print('last layer output shape:  ', last_layer.output_shape)
-    last_output = last_layer.output
-
-    # Build the layers of our own model underneath the pre-existing layers
-    # Flatten captured output layer to 1 dimension
-    my_model_layers = layers.Flatten()(last_output)
-    # Now let's add a fully connected layer with 1024 hidden neurons and ReLU activation
-    my_model_layers = layers.Dense(1024, activation='relu')(my_model_layers)
-    # Let's add a final sigmoid layer for classification
-    my_model_layers = layers.Dense(1, activation='sigmoid')(my_model_layers)
-
-    model = Model(pre_trained_model.input, my_model_layers)
-
-    model.compile(optimizer=RMSprop(learning_rate=0.001), loss='binary_crossentropy', metrics=['acc'])
-
-    validation_datagen = ImageDataGenerator(rescale=1 / 255)
-
-    train_datagen = ImageDataGenerator(
-        rescale=1 / 255,
-        rotation_range=40,  # rotate each image randomly up to 40deg left or right
-        width_shift_range=0.2,  # translate each image up to 20% along X axis
-        height_shift_range=0.2,  # translate each image up to 20% along Y axis
-        shear_range=0.2,  # shear each image up to 20%
-        zoom_range=0.2,  # zoom each image up to 20%
-        horizontal_flip=True,  # randomly flip the image horizontally or vertically
-        fill_mode='nearest'  # fill any missing pixels after a move or shear with the nearest neighbors
-    )
-
-    training_dir = "horse-or-human/training"
-    validation_dir = "horse-or-human/validation"
-
-    train_generator = train_datagen.flow_from_directory(
-        training_dir,
-        target_size=(150, 150),
-        class_mode='binary'
-    )
-
-    validation_generator = validation_datagen.flow_from_directory(
-        validation_dir,
-        target_size=(150, 150),
-        class_mode='binary'
-    )
-
-    model.fit(
-        train_generator,
-        epochs=15,
-        callbacks=[callbacks],
-        validation_data=validation_generator
-    )
-
-    test_images(model=model, kernel=(150, 150))
-
-
 if __name__ == '__main__':
-    # image_augmentation()
-    transfer_learning()
+    main()
